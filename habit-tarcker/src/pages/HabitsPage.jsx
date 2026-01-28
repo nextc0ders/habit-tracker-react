ed939e019e17869bd8d7a3c7df316c0e5a963f87
418ERT <418ert.dev@gmail.com>
2026-01-28 17:26:13 +0400

    chapter 7 - update styles

--- habit-tarcker/src/pages/HabitsPage.jsx
+++ habit-tarcker/src/pages/HabitsPage.jsx
@@ -11,6 +11,8 @@ function HabitsPage() {
 		addHabit
 	} = useHabits()
 
+	const today = new Date().toISOString().slice(0, 10)
+
 	return (
 		<>
 			<h1>Мои привычки</h1>
@@ -27,18 +29,24 @@ function HabitsPage() {
 				<button type="submit">Добавить</button>
 			</form>
 
-			{habits.map(habit => (
-				<div key={habit.id}>
-					<input
-						type='checkbox'
-						checked={habit.completedDates.includes(
-							new Date().toISOString().slice(0, 10)
-						)}
-						onChange={() => toggleHabit(habit.id)}
-					/>
-					{habit.title}
-				</div>
-			))}
+			<ul>
+				{habits.map(habit => {
+					const completed = habit.completedDates.includes(today)
+
+					return (
+						<li key={habit.id} className={completed ? 'completed' : ''}>
+							<label className='habit-item'>
+								<input
+									type='checkbox'
+									checked={completed}
+									onChange={() => toggleHabit(habit.id)}
+								/>
+								<span>{habit.title}</span>
+							</label>
+						</li>
+					)
+				})}
+			</ul>
 		</>
 	)
 }
